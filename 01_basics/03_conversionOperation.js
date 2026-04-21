f06600527409151f841938e64a5f96f75bc3f6a6
asmatullah-md <asmatullahmd922@gmail.com>
2026-04-21 16:44:31 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score = "as12"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "as12" => NaN
+// true => 1
+
+let IsLoggedIn = 1
+
+let booleanIsLoggesIn = Boolean(IsLoggedIn)
+//console.log(booleanIsLoggesIn);
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
