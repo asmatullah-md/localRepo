a83ff1594ea9a61c38be7ef99b8b1557053923a4
asmatullah-md <asmatullahmd922@gmail.com>
2026-04-23 17:44:41 +0000

    dataType

---
+++ datatype_summary.js
@@ -0,0 +1,33 @@
+// premitive
+
+// string, Number, Boolean, null, undefined, Symbole, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const islogged = false
+const outsidetemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+//console.log(id === anotherId);
+
+const bidNumber = 8765331161619n
+
+
+// Reference (Non primitive)
+
+//Array, Objects, Functions
+
+const heros = ["Ironman", "Thor", "BlackWidow"];
+let myObj = {
+    name: "asmat",
+    age: 22,
+}
+const myFunction = function(){
+    console.log("Hello world");
+    
+}
+
+console.log(typeof bigNumber);
