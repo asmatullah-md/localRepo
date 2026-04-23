2fb37bba9a4e51a32685dd5c6a7608cd0ec54e4d
asmatullah-md <asmatullahmd922@gmail.com>
2026-04-23 17:06:36 +0000

    modify

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -19,6 +19,24 @@ let booleanIsLoggesIn = Boolean(IsLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
+// ********************* Operation ***************************
+
+let value = 3
+let negvalue = -value
+//console.log(negvalue);
+
+let str1 = "hello"
+let str2 = " asmat"
+let str3 = str1 + str2
+//console.log(str3);
+
+// console.log(1+2);
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 +2);
+// console.log(1 + 2 + "2");
+
+// frefix and postfix
